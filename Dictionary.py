765552b3e50a23564404d56c908aef926e4d3938
Ray Lee <Lee31978@gmail.com>
2018-02-10 22:05:11 -0500

    one last little update.

--- Dictionary.py
+++ Dictionary.py
@@ -1,7 +1,7 @@
 self = {"name":"Ray", "age":"39", "country of birth":"The United States", "favorite language":"Python"}
+                                            #dictionary created with literal notation above
+def who(tiona):                             #set function
+     for key, data in tiona.items():        #loop through keys and values initialized
+         print "My", key, "is", data + "."  #print statement for each key,value pair formatted
 
-def who(tiona   ):
-     for key, data in tiona.items():
-         print "My", key, "is", data + "."
-
-who(self)
+who(self)                                   #function called 
