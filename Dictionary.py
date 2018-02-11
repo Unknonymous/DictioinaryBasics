75253c90329a9357cf83087764b6600132fa7de0
Ray Lee <Lee31978@gmail.com>
2018-02-10 22:01:32 -0500

    first and final commit for the Diictionary Basics assignment. pY

---
+++ Dictionary.py
@@ -0,0 +1,7 @@
+self = {"name":"Ray", "age":"39", "country of birth":"The United States", "favorite language":"Python"}
+
+def who(tiona   ):
+     for key, data in tiona.items():
+         print "My", key, "is", data + "."
+
+who(self)
